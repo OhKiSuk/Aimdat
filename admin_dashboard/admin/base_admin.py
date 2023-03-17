80a27522c73b298668ff07aef9fc425dd64bc682
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-03-17 15:02:13 +0900

    feat: admin_dashboard to add marketing statistics function
    1. 마케팅 통계 분석 기능 추가

--- admin_dashboard/admin/base_admin.py
+++ admin_dashboard/admin/base_admin.py
@@ -7,11 +7,21 @@ from admin_dashboard.models import InquiryAnswer
 from admin_dashboard.admin.inquiry_answer_admin import InqueryAnswerAdmin
 from admin_dashboard.admin.user_admin import AccountManageAdmin
 from django.contrib.admin import AdminSite
+from django.urls import path
 from services.models.corp_id import CorpId
 from .corp_id_admin import CorpIdAdmin
+from ..views.marketing_views import MarketingView
 
 class CustomAdminSite(AdminSite):
     index_template = 'admin_dashboard/index.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('marketing/statistics', custom_admin_site.admin_view(MarketingView.as_view()) , name='marketing'),
+        ]
+
+        return custom_urls + urls
     
 custom_admin_site = CustomAdminSite(name='custom_admin')
 
