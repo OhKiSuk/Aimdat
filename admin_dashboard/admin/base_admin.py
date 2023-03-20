ea2e9ca513af2525d5dfb446c26eef51548a3159
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-03-21 00:40:01 +0900

    refector: change inquiry_manage can access in view
    1. ModelAdmin에서 작동하던 1:1문의 관리 기능을 view로 이전

--- admin_dashboard/admin/base_admin.py
+++ admin_dashboard/admin/base_admin.py
@@ -1,15 +1,14 @@
 """
-@modified at 2023.03.11
+@modified at 2023.03.20
 @author OKS in Aimdat Team
 """
 from account.models import User
-from admin_dashboard.models import InquiryAnswer
-from admin_dashboard.admin.inquiry_answer_admin import InqueryAnswerAdmin
 from admin_dashboard.admin.user_admin import AccountManageAdmin
 from django.contrib.admin import AdminSite
 from django.urls import path
 from ..views.corp_manage_views import CorpManageView, CorpIdChangeView, CorpInfoChangeView, CorpSummaryFinancialStatementsChangeView
 from ..views.marketing_views import MarketingView
+from ..views.inquiry_manage_views import InquiryListView, InquiryAddAnswerView
 
 class CustomAdminSite(AdminSite):
     index_template = 'admin_dashboard/index.html'
@@ -22,6 +21,10 @@ class CustomAdminSite(AdminSite):
             path('corp/manage/id/<int:pk>/', custom_admin_site.admin_view(CorpIdChangeView.as_view()), name='corp_id_change'),
             path('corp/manage/info/<int:pk>/', custom_admin_site.admin_view(CorpInfoChangeView.as_view()), name='corp_info_change'),
             path('corp/manage/summary/<int:pk>/', custom_admin_site.admin_view(CorpSummaryFinancialStatementsChangeView.as_view()), name='corp_summary_change'),
+
+            #문의사항 관리
+            path('inquiry/manage', custom_admin_site.admin_view(InquiryListView.as_view()), name='inquiry_manage'),
+            path('inquiry/manage/add/answer/<int:pk>', custom_admin_site.admin_view(InquiryAddAnswerView.as_view()), name='add_inquiry_answer')
         ]
 
         return custom_urls + urls
@@ -30,4 +33,3 @@ custom_admin_site = CustomAdminSite(name='custom_admin')
 
 # ModelAdmin 등록
 custom_admin_site.register(User, AccountManageAdmin)
-custom_admin_site.register(InquiryAnswer, InqueryAnswerAdmin)
