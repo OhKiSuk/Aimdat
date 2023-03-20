6e2aeec4b5858584101adfbcb60d600cdf0e09f9
ckdtjq1121 <80ckdtjq@naver.com>
2023-03-21 00:08:29 +0900

    feat : addtional info 1. 거래량, 거래대금, 전일가대비금액, 전일가대비비율 필드추가 2. 주석 추가

--- services/models/stock_price.py
+++ services/models/stock_price.py
@@ -7,12 +7,16 @@ from .corp_id import CorpId
 
 class StockPrice(models.Model):
     corp_id = models.ForeignKey(CorpId, on_delete=models.CASCADE, related_name='corp_id')
-    trade_date = models.DateField()
-    open_price = models.DecimalField(max_digits=16, decimal_places=6)
-    high_price = models.DecimalField(max_digits=16, decimal_places=6)
-    low_price = models.DecimalField(max_digits=16, decimal_places=6)
-    close_price = models.DecimalField(max_digits=16, decimal_places=6)
-    low_price_52 = models.DecimalField(max_digits=16, decimal_places=6)
-    high_price_52 = models.DecimalField(max_digits=16, decimal_places=6)
-    total_stock = models.DecimalField(max_digits=16, decimal_places=6)
-    market_capitalization = models.DecimalField(max_digits=19, decimal_places=2)
+    trade_date = models.DateField() # 거래기준일
+    open_price = models.DecimalField(max_digits=16, decimal_places=6) # 시가
+    high_price = models.DecimalField(max_digits=16, decimal_places=6) # 고가
+    low_price = models.DecimalField(max_digits=16, decimal_places=6) # 저가
+    close_price = models.DecimalField(max_digits=16, decimal_places=6) # 종가
+    low_price_52 = models.DecimalField(max_digits=16, decimal_places=6) # 52주 저가
+    high_price_52 = models.DecimalField(max_digits=16, decimal_places=6) # 52주 고가
+    total_stock = models.DecimalField(max_digits=16, decimal_places=6) # 총주식수
+    market_capitalization = models.DecimalField(max_digits=19, decimal_places=2) # 시가총액
+    trade_quantity = models.DecimalField(max_digits=16, decimal_places=6) # 거래량
+    trade_price = models.DecimalField(max_digits=16, decimal_places=6) # 거래대금
+    change_price = models.DecimalField(max_digits=16, decimal_places=6) # 전일가대비금액
+    change_rate = models.DecimalField(max_digits=16, decimal_places=6) # 전일가대비비율
