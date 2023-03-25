7b2aef7baf16a7557fd81989dccadb2183671280
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-03-25 13:23:24 +0900

    refector: add user foreignkey in inquiry model
    1. inquiry모델에 누락된 user foreignkey 추가

--- services/models/inquiry.py
+++ services/models/inquiry.py
@@ -1,12 +1,17 @@
 """
 @created at 2023.03.12
 @author OKS in Aimdat Team
+
+@modified at 2023.03.25
+@author OKS in Aimdat Team
 """
+from account.models import User
 from django.db import models
 from tinymce.models import HTMLField
 
 # Create your models here.
 class Inquiry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     email = models.EmailField(max_length=255)
     title = models.CharField(max_length=255)
     inquiry_category = models.CharField(max_length=255)
