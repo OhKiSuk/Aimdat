70e3f060d2342655d44813583a927f0841c71b46
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-03-29 14:58:23 +0900

    refector: can access only password resetting user
    1. 비밀번호 변경 중인 사용자만 password_reset_done, password_reset_complete 페이지에 접근 할 수 있도록 개선

--- account/views/password_reset_views.py
+++ account/views/password_reset_views.py
@@ -5,7 +5,10 @@
 @modified at 2023.03.29
 @author OKS in Aimdat Team
 """
+import secrets
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordResetDoneView
+from django.http import BadHeaderError
+from django.core.signing import BadSignature, SignatureExpired, TimestampSigner
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from ..forms.password_reset_forms import CustomPasswordResetForm, CustomSetPasswordForm
@@ -16,7 +19,6 @@ class CustomPasswordResetView(PasswordResetView):
     """
     template_name = 'account/password_reset.html'
     email_template_name= 'account/password_reset_email.html'
-    success_url = reverse_lazy('account:password_reset_done')
     form_class = CustomPasswordResetForm
 
     def dispatch(self, request, *args, **kwargs):
@@ -24,6 +26,12 @@ class CustomPasswordResetView(PasswordResetView):
             return redirect('account:login')
 
         return super().dispatch(request, *args, **kwargs)
+    
+    def get_success_url(self):
+        random_token = secrets.token_hex()
+        signer = TimestampSigner()
+        self.request.session['reset_token'] = signer.sign(random_token)
+        return reverse_lazy('account:password_reset_done')
 
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     """
@@ -34,7 +42,20 @@ class CustomPasswordResetDoneView(PasswordResetDoneView):
     def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('account:login')
+        
+        reset_token = self.request.session.get('reset_token')
+        if not reset_token:
+            raise BadHeaderError('잘못된 접근입니다.')
+        
+        try:
+            signer = TimestampSigner()
+            signer.unsign(reset_token, max_age=300)
+        except SignatureExpired:
+            raise BadHeaderError('잘못된 접근입니다.')
+        except BadSignature:
+            raise BadHeaderError('잘못된 접근입니다.')
 
+        del self.request.session['reset_token']
         return super().dispatch(request, *args, **kwargs)
 
 class CustomPasswordConfirmView(PasswordResetConfirmView):
@@ -42,9 +63,14 @@ class CustomPasswordConfirmView(PasswordResetConfirmView):
     서비스 패스워드 재설정 뷰
     """
     template_name = 'account/password_reset_confirm.html'
-    success_url = reverse_lazy('account:password_reset_complete')
     form_class = CustomSetPasswordForm
 
+    def get_success_url(self):
+        random_token = secrets.token_hex()
+        signer = TimestampSigner()
+        self.request.session['reset_token'] = signer.sign(random_token)
+
+        return reverse_lazy('account:password_reset_complete')
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     """
     패스워드 초기화 완료 후 로그인 뷰
@@ -54,5 +80,18 @@ class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('account:login')
+        
+        reset_token = self.request.session.get('reset_token')
+        if not reset_token:
+            raise BadHeaderError('잘못된 접근입니다.')
+        
+        try:
+            signer = TimestampSigner()
+            signer.unsign(reset_token, max_age=300)
+        except SignatureExpired:
+            raise BadHeaderError('잘못된 접근입니다.')
+        except BadSignature:
+            raise BadHeaderError('잘못된 접근입니다.')
 
+        del self.request.session['reset_token']
         return super().dispatch(request, *args, **kwargs)
