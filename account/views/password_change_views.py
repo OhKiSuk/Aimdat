088d1577eac04892260fc7972fa6b2f184a9e26f
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-03-30 09:53:22 +0900

    refector: only users who are changing password can access
    1. 비밀번호를 변경 중인 사용자만 비밀번호 변경 완료 페이지에 접근할 수 있도록 개선

--- account/views/password_change_views.py
+++ account/views/password_change_views.py
@@ -1,8 +1,14 @@
 """
 @created at 2023.03.05
 @author OKS in Aidmat Team
+
+@modified at 2023.03.30
+@author OKS in Aimdat Team
 """
+import secrets
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.core.signing import BadSignature, SignatureExpired, TimestampSigner
+from django.http import BadHeaderError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
@@ -11,13 +17,18 @@ class CustomPasswordChangeView(PasswordChangeView):
     비밀번호 변경 뷰
     """
     template_name = 'account/password_change.html'
-    success_url = reverse_lazy('account:password_change_done')
 
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('account:login')
 
         return super().dispatch(request, *args, **kwargs)
+    
+    def get_success_url(self):
+        random_token = secrets.token_hex()
+        signer = TimestampSigner()
+        self.request.session['reset_token'] = signer.sign(random_token)
+        return reverse_lazy('account:password_change_done')
 
 class CustomPasswordChangeDoneView(PasswordChangeDoneView):
     """
@@ -28,5 +39,18 @@ class CustomPasswordChangeDoneView(PasswordChangeDoneView):
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('account:login')
+        
+        reset_token = self.request.session.get('reset_token')
+        if not reset_token:
+            raise BadHeaderError('잘못된 접근입니다.')
+        
+        try:
+            signer = TimestampSigner()
+            signer.unsign(reset_token, max_age=300)
+        except SignatureExpired:
+            raise BadHeaderError('잘못된 접근입니다.')
+        except BadSignature:
+            raise BadHeaderError('잘못된 접근입니다.')
 
+        del self.request.session['reset_token']
         return super().dispatch(request, *args, **kwargs)
