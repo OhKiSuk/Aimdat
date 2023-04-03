1cf2d7b51b6994a42a5e16638fbef2a5c5cfd929
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-04-03 15:27:27 +0900

    refector: remove unused email field in inquiry model (#66)
    1. 미사용중인 email field 제거

--- services/models/inquiry.py
+++ services/models/inquiry.py
@@ -2,7 +2,7 @@
 @created at 2023.03.12
 @author OKS in Aimdat Team
 
-@modified at 2023.03.25
+@modified at 2023.04.03
 @author OKS in Aimdat Team
 """
 from account.models import User
@@ -12,7 +12,6 @@ from tinymce.models import HTMLField
 # Create your models here.
 class Inquiry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    email = models.EmailField(max_length=255)
     title = models.CharField(max_length=255)
     inquiry_category = models.CharField(max_length=255)
     content = HTMLField()
