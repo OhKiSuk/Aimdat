c25bdc4eeb4690d20d09f9bb4a7ceabfc4a7200c
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-04-05 01:56:05 +0900

    refector: change dispatch to test_func in delete_account_view
    DeleteAccountView의 명시성 및 가독성 향상을 위해 dispatch 함수를 제거하고 UserPassesTestMixin에서 유효성을 검증하게끔 수정

--- account/views/delete_account_views.py
+++ account/views/delete_account_views.py
@@ -1,28 +1,41 @@
 """
 @created at 2023.04.02
 @author OKS in Aimdat Team
+
+@modified at 2023.04.05
+@author OKS in Aimdat Team
 """
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.views.generic import DeleteView
 
 from ..models import User
 
-class DeleteAccountView(DeleteView):
+class DeleteAccountView(UserPassesTestMixin, DeleteView):
+    http_method_names = ['post']
     model = User
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('index')
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            if self.request.user.is_admin:
+                return False
 
+        return self.request.user.is_authenticated
+    
+    def handle_no_permission(self):
+        return HttpResponseRedirect(reverse_lazy('index'))
+
+    def post(self, request):
         if request.user.user_classify == 'N':
             return redirect('account:naver_login_linkoff')
         elif request.user.user_classify == 'K':
             return redirect('account:kakao_login_linkoff')
         elif request.user.user_classify == 'G':
-            return redirect('account:google_login_linkoff') 
+            return redirect('account:google_login_linkoff')
 
-        return super().dispatch(request, *args, **kwargs)
+        return super().post(request)
 
     def get_object(self, queryset=None):
         return self.request.user
