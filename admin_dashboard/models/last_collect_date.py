62aead97eb16544cd0b6a221ae0d5d7c9c3c98e3
ckdtjq1121 <35961753+ckdtjq1121@users.noreply.github.com>
2023-04-11 23:16:37 +0900

    refactor: last collect date
    1. 2020.01.01 을 default로 지정
    2. dashboard에서 마지막 수집일 확인 가능

--- admin_dashboard/models/last_collect_date.py
+++ admin_dashboard/models/last_collect_date.py
@@ -1,11 +1,12 @@
 """
-@created at 2023.04.05
+@modified at 2023.04.05
 @author cslee in Aimdat Team
 """
+from datetime import datetime
 from django.db import models
-
 # Create your models here.
 class LastCollectDate(models.Model):
-    last_corp_collect_date = models.DateField()
-    last_stock_collect_date = models.DateField()
-    last_summaryfs_collect_date = models.DateField()
+    default_day = datetime(2020, 1, 1)
+    last_corp_collect_date = models.DateField(default=default_day) # 마지막 기업정보 수집일
+    last_stock_collect_date = models.DateField(default=datetime.today().date()) # 마지막 주식정보 수집일
+    last_summaryfs_collect_date = models.DateField(default=default_day) # 마지막 재무정보 수집일
--- admin_dashboard/views/collect_data_views.py
+++ admin_dashboard/views/collect_data_views.py
@@ -3,35 +3,46 @@ author: cslee
 """
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from admin_dashboard.models.last_collect_date import LastCollectDate
 from admin_dashboard.modules.collect.corp import collect_corp
-from admin_dashboard.modules.collect.stock import collect_stockPrice
-from admin_dashboard.modules.collect.summaryFS import collect_fs_data
+from admin_dashboard.modules.collect.stock_price import collect_stock_price
+from admin_dashboard.modules.collect.summary_finaicial_statements import collect_summary_finaicial_statements
+
 
 class CollectCorpInfoView(TemplateView):
     template_name = 'admin_dashboard/data_collect/collect_corp_info.html'
-
+    context = {
+        'last_corp_collect_date' : LastCollectDate.objects.get().last_corp_collect_date,
+    }   
+    
     def get(self, request): # get_corp_info
         tab = request.GET.get('tab', 'none_action')
         if tab == 'collect':
-            collect_corp()
+            collect_corp()     
 
-        return render(self.request, self.template_name)
+        return render(self.request, self.template_name, context=self.context)
     
 class CollectStockPriceView(View):
     template_name = 'admin_dashboard/data_collect/collect_stock_price.html'
+    context = {
+        'last_stock_collect_date' : LastCollectDate.objects.get().last_stock_collect_date
+    }
 
     def get(self, request): # get_stock_price
         tab = request.GET.get('tab', 'none_action')
         if tab == 'collect':
-            collect_stockPrice()
+            collect_stock_price()
             
-        return render(self.request, self.template_name)
+        return render(self.request, self.template_name, context=self.context)
 
 class CollectFinancialStatementView(View):
     template_name = 'admin_dashboard/data_collect/collect_summary_financial_statements.html'
+    context = {
+        'last_summaryfs_collect_date' : LastCollectDate.objects.get().last_summaryfs_collect_date
+    }
 
     years = [2020, 2021, 2022, 2023]
-    quarters = [3, 6, 9, 12]
+    quarters = [1, 2, 3, 4]
     def post(self, request):
         if request.method == 'POST':
             year = request.POST.get('year')
@@ -48,11 +59,11 @@ class CollectFinancialStatementView(View):
             else:
                 choice_quarter.append(self.quarters[int(quarter)-1])
             # 수집 실행
-            for i in choice_year:
-                for j in choice_quarter:
-                    collect_fs_data(i, j)
+            for year in choice_year:
+                for quarter in choice_quarter:
+                    collect_summary_finaicial_statements(year, quarter)
     
-        return render(self.request, self.template_name)
+        return render(self.request, self.template_name, context=self.context)
 
     def get(self, request):
-        return render(self.request, self.template_name)
+        return render(self.request, self.template_name, context=self.context)
