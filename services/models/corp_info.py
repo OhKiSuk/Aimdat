2bd6afd52ef682dca2c8431082157ae3f4a352bc
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-06-19 17:18:51 +0900

    fix: can't update corp_info cause of url field type
    url 필드 타입 문제로 인해 기업정보가 갱신이 안되는 문제 수정

--- services/models/corp_info.py
+++ services/models/corp_info.py
@@ -1,13 +1,16 @@
 """
 @created at 2023.03.15
 @author OKS in Aimdat Team
+
+@modified at 2023.06.19
+@author OKS in Aimdat Team
 """
 from django.db import models
 from .corp_id import CorpId
 
 class CorpInfo(models.Model):
     corp_id = models.OneToOneField(CorpId, models.CASCADE, related_name='corp_info')
-    corp_homepage_url = models.URLField(null=True)
+    corp_homepage_url = models.CharField(max_length=255, null=True)
     corp_settlement_month = models.CharField(max_length=255, null=True) # 결산월
     corp_ceo_name = models.CharField(max_length=255, null=True)
     corp_summary = models.TextField(null=True)
