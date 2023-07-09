d0a4b79aeb4ea6f743137934e0ed323d2d3297b4
ohkiseok <44806776+OhKiSuk@users.noreply.github.com>
2023-07-09 14:20:13 +0900

    fix: disclosure paging error and forget show marketcorp
    공시 페이징에러 및 빼먹은 시가총액 값 가독성 향상 커밋 추가

---
+++ services/templatetags/divide_marketcap.py
@@ -0,0 +1,34 @@
+"""
+@created at 2023.07.09
+@author OKS in Aimdat Team
+"""
+from decimal import (
+    Context,
+    Decimal
+)
+from django import template
+
+register = template.Library()
+
+@register.filter(name='divide_marketcap')
+def divide_marketcap(obj):
+    ctx = Context(prec=4)
+
+    obj_to_decimal = Decimal(obj)
+
+    if obj_to_decimal >= Decimal(100_000_000_000):
+        result = ctx.divide(obj_to_decimal, Decimal(100_000_000_000))
+        trillion = str(result).split('.')[0]
+        billion = str(result).split('.')[1]
+
+        if int(billion) < 1:
+            return trillion+'조'
+        else:
+            return trillion+'조 '+billion+'억'
+    elif obj_to_decimal >= Decimal(100_000_000):
+        result = ctx.divide(obj_to_decimal, Decimal(100_000_000))
+        billion = str(result).split('.')[0]
+
+        return billion+'억'
+    else:
+        return obj_to_decimal
